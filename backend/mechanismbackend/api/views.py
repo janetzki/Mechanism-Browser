132f839633a9ff932fd2aaacfe338b799b37fc72
alpreu <11444089+alpreu@users.noreply.github.com>
2018-04-23 15:35:41 +0200

    adding hardcoded mechanisms for demo

--- backend/mechanismbackend/api/views.py
+++ backend/mechanismbackend/api/views.py
@@ -4,7 +4,8 @@ from rest_framework.decorators import api_view
 
 
 
-hardcoded_json = """{"wormdrive": { 
+hardcoded_json = """{
+"wormdrive": { 
 "link": "https://grabcad.com/library/customisable-worm-transmission-1", 
 "image": "/img/0001-wormdrive.jpg", 
 "input": [ 
@@ -26,7 +27,56 @@ hardcoded_json = """{"wormdrive": {
 "transmission": 100, 
 "comments": [], 
 "metadata": []
-}}"""
+},
+
+"bevelgears": {
+"link": "https://grabcad.com/library/parametric-bevel-gear-set-for-nx-1",
+"image": "/img/0002-bevelgears.jpg",
+"input": [
+{   "r1": 1 },
+{   "r2": 0 },
+{   "r3": 0 },
+{   "t1": 0 },
+{   "t2": 0 },
+{   "t3": 0 }
+],
+"output": [
+{   "r1": 0 },
+{   "r2": 1 },
+{   "r3": 0 },
+{   "t1": 0 },
+{   "t2": 0 },
+{   "t3": 0 }
+],
+"transmission": 10,
+"comments": [],
+"metadata": []
+},
+
+"beltdrive-quarterturn": {
+"link": "https://www.youtube.com/watch?v=RpVSn_ZZCOI",
+"image": "/img/0003-beltdrive-quarterturn.jpg",
+"input": [
+{   "r1": 1 },
+{   "r2": 0 },
+{   "r3": 0 },
+{   "t1": 0 },
+{   "t2": 0 },
+{   "t3": 0 }
+],
+"output": [
+{   "r1": 0 },
+{   "r2": 1 },
+{   "r3": 0 },
+{   "t1": 0 },
+{   "t2": 0 },
+{   "t3": 0 }
+],
+"transmission": 1,
+"comments": [],
+"metadata": []
+}
+}"""
 
 
 @api_view(['GET'])
