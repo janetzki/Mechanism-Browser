d4af844816e33bb42cea24eacab1af9254da5590
alpreu <11444089+alpreu@users.noreply.github.com>
2018-04-23 15:28:10 +0200

    changing path for mechanisms

--- backend/mechanismbackend/api/urls.py
+++ backend/mechanismbackend/api/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url, include
 from . import views
 
 urlpatterns = [
-    #url(r'^mechanisms/$', 'mechanism_collection')
-    url(r'^$', views.api_root),
+    #url(r'^mechanisms/$', 'mechanism_collection'),
+    url(r'^mechanisms/$', views.api_root),
+    #url(r'^$', views.api_root),
 ]
