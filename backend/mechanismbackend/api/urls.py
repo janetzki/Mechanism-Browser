415c7417e69534cc985983c7180c5c75464da2b9
alpreu <11444089+alpreu@users.noreply.github.com>
2018-04-26 22:57:07 +0200

    migrating to drf browsable api views

--- backend/mechanismbackend/api/urls.py
+++ backend/mechanismbackend/api/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url, include
 from . import views
 
 urlpatterns = [
-    url(r'^mechanisms/$', views.mechanism_list),
-    url(r'^mechanisms/(?P<pk>[0-9]+)/$', views.mechanism_detail),
+    url(r'^mechanisms/$', views.MechanismList.as_view()),
+    url(r'^mechanisms/(?P<pk>[0-9]+)/$', views.MechanismDetail.as_view()),
 ]
--- backend/mechanismbackend/api/views.py
+++ backend/mechanismbackend/api/views.py
@@ -1,55 +1,16 @@
-from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework.renderers import JSONRenderer
-from rest_framework.parsers import JSONParser
+from rest_framework import generics
 from .models import Mechanism
 from .serializers import MechanismSerializer
 
 
-@csrf_exempt
-def mechanism_list(request):
-    """
-    List all mechanisms, or create a new one.
-    """
-    if request.method == 'GET':
-        mechanisms = Mechanism.objects.all()
-        serializer = MechanismSerializer(mechanisms, many=True)
-        return JsonResponse(serializer.data, safe=False)
-
-    elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = MechanismSerializer(data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=400)
+class MechanismList(generics.ListAPIView):
+    queryset = Mechanism.objects.all()
+    serializer_class = MechanismSerializer
 
 
-@csrf_exempt
-def mechanism_detail(request, pk):
-    """
-    Retrieve, update or delete a mechanism.
-    """
-    try:
-        mechanism = Mechanism.objects.get(pk=pk)
-    except Mechanism.DoesNotExist:
-        return HttpResponse(status=404)
+class MechanismDetail(generics.RetrieveAPIView):
+    queryset = Mechanism.objects.all()
+    serializer_class = MechanismSerializer
 
-    if request.method == 'GET':
-        serializer = MechanismSerializer(mechanism)
-        return JsonResponse(serializer.data)
-
-    elif request.method == 'PUT':
-        data = JSONParser().parse(request)
-        serializer = MechanismSerializer(mechanism, data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return JsonResponse(serializer.data)
-        return JsonResponse(serializer.errors, status=400)
-
-    elif request.method == 'DELETE':
-        mechanism.delete()
-        return HttpResponse(status=204)
 
 
