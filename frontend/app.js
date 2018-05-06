318e9f12e97c5e35f90146bc14527654e6113a7b
Jonathan Janetzki <jonathan.janetzki@student.hpi.de>
2018-05-06 19:22:52 +0200

    :art: Close #8: Add templating engine mustache.js

--- frontend/app.js
+++ frontend/app.js
@@ -1,13 +1,30 @@
-var express = require('express');
-var path = require('path');
+const express = require('express');
+const cons = require('consolidate');
+const path = require('path');
+const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
-var app = express();
-
+const app = express();
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('*', function (req, res) {
-  res.sendFile(__dirname + '/public/index.html');
+app.engine('html', cons.mustache);
+app.set('views', path.join(__dirname + '/public'));
+app.set('view engine', 'html');
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/public/index.html');
 });
 
-app.listen(process.env.PORT || 8080);
+app.get('/mechanism/:id', function (req, res) {
+    const xhttp = new XMLHttpRequest();
+    xhttp.open('GET', 'http://127.0.0.1:8000/api/mechanisms/?id=' + req.params['tagId']);
+    xhttp.setRequestHeader('Content-type', 'application/json');
+    xhttp.send();
+    xhttp.onreadystatechange = function() {
+        if (xhttp.readyState == 4 && xhttp.status == 200) {
+            const response = JSON.parse(xhttp.responseText);
+            res.render('mechanism-article', response[0])
+        }
+    };
+});
 
+app.listen(8080);
