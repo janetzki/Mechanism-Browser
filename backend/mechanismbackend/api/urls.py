d851e8890c7d921b02ae72a9090113f701293b3a
alpreu <11444089+alpreu@users.noreply.github.com>
2018-05-20 12:23:51 +0200

    implementing mechanism creation with POST route

--- backend/mechanismbackend/api/urls.py
+++ backend/mechanismbackend/api/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url, include
+from django.views.decorators.csrf import csrf_exempt
 from . import views
 
 urlpatterns = [
     url(r'^mechanisms/$', views.MechanismList.as_view()),
     url(r'^mechanisms/(?P<pk>[0-9]+)/$', views.MechanismDetail.as_view()),
+    url(r'^mechanisms/create/$', csrf_exempt(views.MechanismCreate.as_view())),
 ]
