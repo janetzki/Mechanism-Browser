60c25fb119bd3a02dab51ee041c9e656b421e2ad
alpreu <11444089+alpreu@users.noreply.github.com>
2018-05-24 17:39:40 +0200

    allowing blank fields, defaulting name to 'Mechanism Request' if not provided

--- backend/mechanismbackend/api/models.py
+++ backend/mechanismbackend/api/models.py
@@ -5,8 +5,8 @@ from django.db import models
 
 class Mechanism(models.Model):
     created = models.DateTimeField(auto_now_add=True)
-    name = models.CharField(max_length=200)
-    link = models.URLField()
+    name = models.CharField(max_length=200, default='Mechanism Request')
+    link = models.URLField(blank=True)
     image = models.ImageField(upload_to='img/', default='img/mechanism-placeholder.png')
     inputR1 = models.BooleanField()
     inputR2 = models.BooleanField()
@@ -20,9 +20,8 @@ class Mechanism(models.Model):
     outputT1 = models.BooleanField()
     outputT2 = models.BooleanField()
     outputT3 = models.BooleanField()
-    transmission = models.IntegerField()  # TODO: what does this represent and is this the right type?
-    comments = models.TextField(blank=True)  # TODO: might be better to use charfield, have to think about this
-    # TODO: what to do with the metadate field in the JSON template?
+    transmission = models.IntegerField(blank=True, null=True)
+    comments = models.TextField(blank=True)
 
     class Meta:
         ordering = ('created',)
