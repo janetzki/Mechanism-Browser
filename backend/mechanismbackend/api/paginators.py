acbbb44646cfc849c34d9b80cfb71328ca9c5189
alpreu <11444089+alpreu@users.noreply.github.com>
2018-05-31 22:40:30 +0200

    including current page number in pagination

--- backend/mechanismbackend/api/paginators.py
+++ backend/mechanismbackend/api/paginators.py
@@ -7,6 +7,7 @@ from rest_framework.utils.urls import replace_query_param
 class FirstLastPageNumberPaginator(pagination.PageNumberPagination):
     def get_paginated_response(self, data):
         return Response(OrderedDict([
+            ('current', self.page.number),
             ('count', self.page.paginator.count),
             ('first', self.get_first_link()),
             ('previous', self.get_previous_link()),
